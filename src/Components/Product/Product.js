934c50bbcf05ef38c108a435bfccd2e99f4b0f8e
nomayen <nomayen.ohin@gmail.com>
2023-03-17 02:33:58 +0600

    data flow

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,6 +3,9 @@ import './Product.css'
 
 const Product = (props) => {
     const{name, img, seller,price, ratings}=props.product;
+   const {handleAddToCart} =props;
+
+    
     return (
         <div className='product'>
            <img src={img} alt=""></img>
@@ -12,7 +15,7 @@ const Product = (props) => {
            <p><small>seller:{seller}</small></p>
            <p><small>ratings:{ratings} stars</small></p>
            </div>
-           <button className='btn-cart'>
+           <button onClick={(()=>handleAddToCart(props.product))} className='btn-cart'>
             <p>Add to cart</p>
            </button>
         </div>
