ee650c9b7b4330d00af7055d6ba05727ef7548c8
nomayen <nomayen.ohin@gmail.com>
2023-03-18 21:51:40 +0600

    order summary price

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} =props;
+    console.log(cart);
+   
+    let total = 0;
+    let shipping =0;
+    for(const product of cart){
+            total=total+product.price;
+            shipping=shipping+product.shipping;
+    }
+    const tax = (total *0.1).toFixed(2)
     return (
-        <div>
+        <div className='cart'>
             <h3>Order summary</h3>
             <p>Selected items {cart.length}</p>
+            <p>Total Price:{total}</p>
+            <p>Total Shipping:{shipping}</p>
+            <p>tax:{tax}</p>
+            <h5>Grand Total</h5>
         </div>
     );
 };
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -12,10 +12,10 @@ const Product = ({product, handleAddToCart}) => {
         <div className='product'>
            <img src={img} alt=""></img>
            <div className='product-info'>
-           <p className='product-name'>{name}</p>
-           <p>Price :{price}</p>
-           <p><small>seller:{seller}</small></p>
-           <p><small>ratings:{ratings} stars</small></p>
+        <p className='product-name'>name{name}</p>
+           <p>Price: ${price}</p>
+           <p>seller: {seller}</p>
+           <p>Reatting: {ratings}</p>
            </div>
            <button onClick={(()=>handleAddToCart(product))} className='btn-cart'>
             <p className='btn-text'>Add to cart</p>
